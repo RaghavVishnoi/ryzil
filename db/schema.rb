7806dee31f636eb39110169c7523906720b15723
RaghavVishnoi <raghavvishnoi10@gmail.com>
2017-09-05 23:16:56 +0530

    Update logo dimensions

--- db/schema.rb
+++ db/schema.rb
@@ -25,4 +25,9 @@ ActiveRecord::Schema.define(version: 20170825100034) do
     t.datetime "updated_at",     null: false
   end
 
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
