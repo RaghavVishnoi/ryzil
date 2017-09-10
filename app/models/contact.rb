bb71f25878d6e079c7dabecdff8dd561e6cf76dc
RaghavVishnoi <raghavvishnoi10@gmail.com>
2017-09-10 14:42:03 +0530

    Remove callback to send email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,18 +1,10 @@
 class Contact < ActiveRecord::Base
 
-  after_create :notify_mail
-
   validates :name, presence: true
   validates :email, presence: true
   validates :contact_number, presence: true
   validates :subject, presence: true
   validates :message, presence: true
 
-
-private
-    def notify_mail
-    	puts "ininin"
-		ContactMailer.contacts(self).deliver_now
-	end
 
 end
